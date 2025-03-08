205959e91aa4ee365f0dd9890ff8abaf26179a8c
Ethan Meli <etme6835@colorado.edu>
2025-03-07 17:14:30 -0700

    Added interests to User model

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
